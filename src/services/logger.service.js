e0c7f1cb126556c28b8a191fe80045eaefca93d2
Asbin Thapa <asbinthapa27@gmail.com>
2026-04-24 19:32:43 +0100

    feat: add src/services/logger.service.js

---
+++ src/services/logger.service.js
@@ -0,0 +1,18 @@
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: process.env.LOG_LEVEL || (process.env.NODE_ENV === 'production' ? 'info' : 'debug'),
+  format: format.combine(
+    format.timestamp(),
+    format.errors({ stack: true }),
+    format.json()
+  ),
+  defaultMeta: {
+    service: 'nepal-api-ecosystem'
+  },
+  transports: [
+    new transports.Console()
+  ]
+});
+
+module.exports = logger;
